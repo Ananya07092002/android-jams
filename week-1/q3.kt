a08538ee018434ff90ce80af30435887ea1feea3
Ananya07092002 <70059236+Ananya07092002@users.noreply.github.com>
2020-12-19 15:47:38 +0530

    Create q3.kt

---
+++ week-1/q3.kt
@@ -0,0 +1,10 @@
+package com.example.week1assignment
+
+import java.lang.Math.abs
+
+fun main() {
+    println("Enter a number:")
+    val x=Integer.valueOf(readLine())
+    val y=abs(x)
+    println("The absolute value of the given number is $y")
+}
